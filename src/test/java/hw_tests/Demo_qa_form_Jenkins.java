def68779adda3beaacaf98e7244e320c1b48bbf0
Daniil Glushenkov <glushenkov.daniel@yandex.ru>
2022-08-30 21:00:23 +0300

    + added lyambda steps for allure report

--- src/test/java/hw_tests/Demo_qa_form_Jenkins.java
+++ src/test/java/hw_tests/Demo_qa_form_Jenkins.java
@@ -1,74 +1,114 @@
 package hw_tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
 
 public class Demo_qa_form_Jenkins extends TestBase {
 
+    @DisplayName("Проверка корректного заполнения формы")
     @Test
-    void successfulTest1() {
-        String name = "Alex";
+    @Owner("Daniil")
+    @Severity(SeverityLevel.TRIVIAL)
+    @Feature("Оформление страховки")
+    @Story("Отправка формы")
+    @Description("Чекаем работу лямбда степов")
+    @Link(name = "demoqa", url = "https://demoqa.com")
+    void successfulTest() {
+        String name = "Daniil";
 
-        open("/automation-practice-form");
-        executeJavaScript("$('footer').remove()");
-        executeJavaScript("$('#fixedban').remove()");
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем страницу с формой и блочим рекламу", () -> {
+            open("/automation-practice-form");
+            executeJavaScript("$('footer').remove()");
+            executeJavaScript("$('#fixedban').remove()");
+        });
 
         // Text
-        $("[id=firstName]").setValue("Daniil"); //  $("#firstName").setValue("Daniil");
-        $("[id=lastName]").setValue("Davydov"); //  $("#firstName").setValue("Davydov");
-        $("[id=userEmail]").setValue("da@da.ru"); //  $("#firstName").setValue("da@da.ru");
+        step("Вводим имя", () -> {
+            $("[id=firstName]").setValue(name);
+        });
+        step("Вводим фамилию", () -> {
+            $("[id=lastName]").setValue("Davydov");
+        });
+        step("Вводим адрес почты", () -> {
+            $("[id=userEmail]").setValue("da@da.ru");
+        });
 
         // Radio-button
-        $(byText("Male")).click(); // $("[for='gender-radio-1']").click(); - аналогично
-        $("[id=userNumber]").setValue("0123456789"); // $("#userNumber").setValue("0123456789");
+        step("Выбираем пол", () -> {
+            $(byText("Male")).click();
+        });
+        step("Вводим номер телефона", () -> {
+            $("[id=userNumber]").setValue("0123456789");
+        });
 
         // Date
-        $("[id=dateOfBirthInput]").click();
-        $(".react-datepicker__month-select").selectOption("August");
-        $(".react-datepicker__year-select").selectOption("1993");
-        $("[aria-label='Choose Tuesday, August 10th, 1993']").click();
+        step("Выбираем дату рождения", () -> {
+            $("[id=dateOfBirthInput]").click();
+            $(".react-datepicker__month-select").selectOption("August");
+            $(".react-datepicker__year-select").selectOption("1993");
+            $("[aria-label='Choose Tuesday, August 10th, 1993']").click();
+        });
 
         // Text
-        $("#subjectsInput").sendKeys("m");
-        $(byText("Maths")).click();
-        // $("#subjectsInput").sendKeys("Maths");
-        // $("#subjectsInput").pressEnter();
+        step("Выбираем предмет", () -> {
+            $("#subjectsInput").sendKeys("m");
+            $(byText("Maths")).click();
+        });
 
         // Check-box
-        $(byText("Sports")).click(); // $("[for='hobbies-checkbox-1']").click();  - аналогично
-        $(byText("Reading")).click(); // $("#hobbiesWrapper").$(byText("Sports")).click();
-        $(byText("Music")).click();
+        step("Выбираем предмет", () -> {
+            $(byText("Sports")).click();
+            $(byText("Reading")).click();
+            $(byText("Music")).click();
+        });
 
         // Picture
-        // $("#uploadPicture").uploadFile (new File("src/test/resources/pic.png"));
-        $("#uploadPicture").uploadFromClasspath("pic.png");
+        step("Выбрать файл", () -> {
+            $("#uploadPicture").uploadFromClasspath("pic.png");
+        });
 
         // Text
-        $("[id=currentAddress]").setValue("Adress"); // $("#currentAddress").setValue("Adress");
+        step("Вводим адрес", () -> {
+            $("[id=currentAddress]").setValue("Adress");
+        });
 
         // Drop-down list
-        $("#state").click();
-        $(byText("NCR")).click();
-        $("#city").click();
-        $(byText("Delhi")).click();
+        step("Выбираем штат и город", () -> {
+            $("#state").click();
+            $(byText("NCR")).click();
+            $("#city").click();
+            $(byText("Delhi")).click();
+        });
 
-        $("[id=submit]").click();
+        step("Нажимаем разместить", () -> {
+            $("[id=submit]").click();
+        });
 
         // Result:
-        $(".modal-body").shouldHave(text("Daniil Davydov"),
-                text("da@da.ru"),
-                text("Male"),
-                text("0123456789"),
-                text("10 August,1993"),
-                text("Maths"),
-                text("Sports, Reading, Music"),
-                text("pic.png"),
-                text("Adress"),
-                text("NCR Delhi")
-        );
+        step("Проверяем результат", () -> {
+            $(".modal-body").shouldHave(text("Daniil Davydov"),
+                    text("da@da.ru"),
+                    text("Male"),
+                    text("0123456789"),
+                    text("10 August,1993"),
+                    text("Maths"),
+                    text("Sports, Reading, Music"),
+                    text("pic.png"),
+                    text("Adress"),
+                    text("NCR Delhi")
+            );
+        });
+
     }
 }
 
