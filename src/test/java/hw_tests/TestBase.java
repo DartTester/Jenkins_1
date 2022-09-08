f2e576582072a055527a2c5dcdec3af1111c6338
Daniil Glushenkov <glushenkov.daniel@yandex.ru>
2022-09-08 23:25:49 +0300

    + add owner

--- src/test/java/hw_tests/TestBase.java
+++ src/test/java/hw_tests/TestBase.java
@@ -1,10 +1,12 @@
 package hw_tests;
 
+import Configs.CredentialsConfig;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 //import helpers.Attach;
 import hw_helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -22,9 +24,19 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = System.getProperty("browser","chrome");
+        Configuration.browserVersion = System.getProperty("version","100");
+        Configuration.browserSize = System.getProperty("browserSize", "3840x2160");
+
+        //Логин и пароль получаем из файла, которого нет в Гите и который мы создаем прямо в Дженкинсе
+        CredentialsConfig credentialsConfig = ConfigFactory.create(CredentialsConfig.class);
+        Configuration.remote = "https://" + credentialsConfig.login() + ":" + credentialsConfig.password() + "@" +
+                credentialsConfig.urlCloudSelenoid();
+
         // Configuration.browserSize = "1920x1080";
+
         // Надо указать, чтобы тесты запускались не в локальном браузере, а в Selenoid:
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
